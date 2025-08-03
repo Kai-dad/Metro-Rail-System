33cde5e26a604703d14fa747ec405c542a4ec2c9
Kai-dad <nsovonkuna98@gmail.com>
2025-08-03 19:23:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -657,6 +657,29 @@ function filterRows() {
   });
 }
 
+//AUDREY JS
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+
+  // Hide all slides
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  // Move to the next slide
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+
+  // Show current slide
+  slides[slideIndex - 1].style.display = "block";
+
+  // Change image every 5 seconds
+  setTimeout(showSlides, 5000);
+}
+
 // =============================================
 // INITIALIZE APPLICATION
 // =============================================
