b91032d61bc254aaa8b673a6cfe40b549630cdba
Kai-dad <nsovonkuna98@gmail.com>
2025-08-18 21:57:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ let routeInterval;
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB2gjql42QQAn6kEnuAlb-U8uO4veOf9kQ",
+  authDomain: "metro-rail-2de9c.firebaseapp.com",
+  projectId: "metro-rail-2de9c",
+  storageBucket: "metro-rail-2de9c.firebasestorage.app",
+  messagingSenderId: "1036516254492",
+  appId: "1:1036516254492:web:a1d07b16233af9cecc90d9"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
