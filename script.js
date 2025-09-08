3a1c8059513d70c2bb350b8cff42739412355c2c
Kai-dad <nsovonkuna98@gmail.com>
2025-09-08 18:55:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -521,25 +521,24 @@ function setupFAQEvents() {
 // ================= Slideshow =================
 
 let slideIndex = 0;
-
-function initSlideshow() {
-  showSlides();
-}
+showSlides();
 
 function showSlides() {
-  const slides = document.getElementsByClassName("mySlides");
-  const dots = document.getElementsByClassName("dot");
-  
-  for (let i = 0; i < slides.length; i++) slides[i].style.display = "none";
-  
+  let slides = document.getElementsByClassName("slide");
+
+  // Hide all slides
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  // Move to the next slide
   slideIndex++;
-  if (slideIndex > slides.length) slideIndex = 1;
-  
-  for (let i = 0; i < dots.length; i++) dots[i].className = dots[i].className.replace(" active", "");
-  
-  if (slides[slideIndex-1]) slides[slideIndex-1].style.display = "block";  
-  if (dots[slideIndex-1]) dots[slideIndex-1].className += " active";
-  
+  if (slideIndex > slides.length) { slideIndex = 1 }
+
+  // Show current slide
+  slides[slideIndex - 1].style.display = "block";
+
+  // Change image every 5 seconds
   setTimeout(showSlides, 5000);
 }
 
