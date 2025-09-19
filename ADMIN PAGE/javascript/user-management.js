2f2468459ecf92d66c5dfe3c27a11e629181be33
audrey2007-hash <audreymabhena20@gmail.com>
2025-09-19 14:37:12 +0200

    Update user-management.js

--- ADMIN PAGE/javascript/user-management.js
+++ ADMIN PAGE/javascript/user-management.js
@@ -3,8 +3,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyB2gjql42QQAn6kEnuAlb-U8uO4veOf9kQ",
   authDomain: "metro-rail-2de9c.firebaseapp.com",
   projectId: "metro-rail-2de9c",
-  storageBucket: "metro-rail-2de9c.firebasestorage.app",
-  //storageBucket: "metro-rail-2de9c.appspot.com"
+ // storageBucket: "metro-rail-2de9c.firebasestorage.app",
+  storageBucket: "metro-rail-2de9c.appspot.com"
   messagingSenderId: "1036516254492",
   appId: "1:1036516254492:web:a1d07b16233af9cecc90d9"
 };
