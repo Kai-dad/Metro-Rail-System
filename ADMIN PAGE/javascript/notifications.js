f101e434857ee1c45fc48ed782ca290ece513dd5
Kagiso Matjomane <202317261@swave.smu.ac.za>
2025-09-24 16:44:02 +0200

    Update notifications.js

--- ADMIN PAGE/javascript/notifications.js
+++ ADMIN PAGE/javascript/notifications.js
@@ -1,3 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB2gjql42QQAn6kEnuAlb-U8uO4veOf9kQ",
+  authDomain: "metro-rail-2de9c.firebaseapp.com",
+  projectId: "metro-rail-2de9c",
+  storageBucket: "metro-rail-2de9c.firebasestorage.app",
+  messagingSenderId: "1036516254492",
+  appId: "1:1036516254492:web:a1d07b16233af9cecc90d9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
 document.addEventListener('DOMContentLoaded', function() {
   
   const notifications = [
@@ -250,3 +263,4 @@ document.getElementById('notification-form').addEventListener('submit', function
     alert('Error sending notification. Please try again.');
   });
 });
+
